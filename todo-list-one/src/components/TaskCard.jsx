be3e9f4297e47431dac3edb09b4ae7d9271259d9
VladyBarvy <vladbarskoff@yandex.ru>
2024-11-24 22:19:25 +0300

    241124 modifi task-list

--- todo-list-one/src/components/TaskCard.jsx
+++ todo-list-one/src/components/TaskCard.jsx
@@ -1,6 +1,28 @@
 import React, { useState } from 'react';
 import EditTaskModal from './EditTaskModal';
 import ConfirmationModal from './ConfirmationModal';
+import '../styles/TaskCard.css';
+
+
+
+
+// Функция для форматирования даты в формат DD/MM/YYYY HH:mm
+const formatDate = (dateString) => {
+  if (!dateString) return '';
+
+  const date = new Date(dateString);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Месяцы начинаются с 0
+  const year = date.getFullYear();
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+
+  return `${day}/${month}/${year} ${hours}:${minutes}`;
+};
+
+
+
+
 
 const TaskCard = ({ task, deleteTask, toggleTaskStatus, updateTask }) => {
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
@@ -24,7 +46,7 @@ const TaskCard = ({ task, deleteTask, toggleTaskStatus, updateTask }) => {
       <div className="task-details">
         <h3>{task.title}</h3>
         <p>{task.description}</p>
-        <p>{task.dueDate}</p>
+        <p>{formatDate(task.dueDate)}</p>
       </div>
       <div className="task-actions">
         <button onClick={() => setIsEditModalOpen(true)}>Редактировать</button>
--- todo-list-one/src/components/TaskList.jsx
+++ todo-list-one/src/components/TaskList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TaskCard from './TaskCard';
+import '../styles/TaskList.css';
 
 const TaskList = ({ tasks, deleteTask, toggleTaskStatus, updateTask }) => {
   return (
